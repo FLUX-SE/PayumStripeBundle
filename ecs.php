09e25ce242d35526943fe746b58fe930cd286033
Francis Hilaire <Prometee@users.noreply.github.com>
2022-11-04 17:32:32 +0100

    Add Symfony 6 support

--- ecs.php
+++ ecs.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__.'/vendor/symplify/easy-coding-standard/config/set/symfony.php');
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->import(__DIR__.'/vendor/symplify/easy-coding-standard/config/set/psr12.php');
 };
