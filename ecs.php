347c5507b8191b1409d5c24c6bce095bc1abb076
Francis Hilaire <Prometee@users.noreply.github.com>
2022-11-04 17:49:55 +0100

    Fix ECS

--- ecs.php
+++ ecs.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->import(__DIR__.'/vendor/symplify/easy-coding-standard/config/set/psr12.php');
+    $ecsConfig->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/psr12.php');
 };
