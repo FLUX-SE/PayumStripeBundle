c4293d4f1b460af1d4f2c506f85872d8fe34d431
Francis Hilaire <Prometee@users.noreply.github.com>
2025-01-09 13:38:30 +0100

    Clean deps and upgrade to Symfony 7

--- ecs.php
+++ ecs.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/psr12.php');
-};
+return ECSConfig::configure()
+    ->withSets([
+        'vendor/symplify/easy-coding-standard/config/set/psr12.php',
+    ])
+    ->withPaths([
+        'src',
+        'tests',
+    ])
+;
